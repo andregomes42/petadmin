7c35cee6fa29fa3977816ef7e587addd469df4a4
costelinha <andregomesngr@gmail.com>
2020-09-24 14:16:28 -0500

    including dependent destroy in sell and client

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,15 +1,16 @@
-class Sell < ApplicationRecord
+class Sell &lt; ApplicationRecord
   include Fae::BaseModelConcern
+
   enum status: { finalizada: 0, cancelada: 1 }
   validates :client, presence: true
 
   belongs_to :discount
   belongs_to :client
 
-  has_many :sell_products
+  has_many :sell_products, dependent: :destroy
   has_many :products, through: :sell_products
 
-  has_many :sell_services
+  has_many :sell_services, dependent: :destroy
   has_many :services, through: :sell_services
 
   before_save :set_total
@@ -25,14 +26,14 @@ class Sell < ApplicationRecord
   private
   def set_total
     total = 0
-    self.products.each { |p| total += p.price }
-    self.services.each { |s| total += s.price }
+    self.products.each {|p| total += p.price }
+    self.services.each {|s| total += s.price }
 
     if self.discount.present?
       total = total - self.discount.value
     end
 
-    total = (total >= 0)? total : 0
+    total = (total &gt;= 0)? total : 0
     self.total = total
   end
 end
